8025abe9e5a8c10d3a6f332ab7a6013df2acbb08
Henryforeman1 <79278940+Henryforeman1@users.noreply.github.com>
2021-03-22 11:00:15 -0400

    Project 5

---
+++ Assignments/Assinment 5/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {0, 2, 4, 20, 7, 22, 9, 65, 100, 17, 84};
+        int sum = 0;
+
+        int sum_array = summation(numbers, sum);
+        {
+            System.out.println("The sum of the list is: " + sum_array);
+        }
+    }
+
+
+    static int summation(int[] numbers, int sum){
+        for (int elem : numbers) {
+            sum += elem;
+        }
+        return sum;
+    }
+}
+
