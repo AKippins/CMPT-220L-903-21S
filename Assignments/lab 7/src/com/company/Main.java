fa4343abce73a002f83bfc3d605d98884b2bb6a3
Henryforeman1 <79278940+Henryforeman1@users.noreply.github.com>
2021-05-03 18:48:53 -0400

    assinments and lab, hope this works

---
+++ Assignments/lab 7/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<listitem> List = new ArrayList<listitem>();
+
+        Scanner george = new Scanner(System.in);
+        System.out.println("please enter the index of item you wish to remove");
+        int index = george.nextInt();
+
+        for (int f=0; f<4;f++) {
+            listitem.add(List);
+        }
+
+        listitem.order(List);
+
+        listitem.delete(index, List);
+
+
+    }
+}
